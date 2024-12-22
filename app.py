eb56ca7c62544e92eb12e7aa2e4d2ebacb3bfc1c
SnowArrow16 <yukato2004@gmail.com>
2024-12-22 22:59:43 +0900

    second push

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+
 #ボタンを押して画像をstatic内に保存されるようにする
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -26,10 +28,14 @@ def upload():
         file.save(filepath)
         return redirect(url_for('change'))
 
+
+
 #change.htmlのエンドポイント
 @app.route('/change')
 def change():
     return render_template('change.html')
+
+
 
 #画像変換ようのエンドポイント
 @app.route('/change/conv',  methods=['POST'])
@@ -37,13 +43,19 @@ def conv():
     nega = negaposi()
     # `select` タグで選択された値を取得
     selected_value = request.form.get('selected_option')
-    if selected_value == "1":
+    if selected_value=="":
+        return '変換方法を選択してください', 400
+    elif selected_value == "1":
         nega.negaposi_ms()
+        conv_message = "画像の画像内の濃淡を入れ替える変換です。画像内の明るい画素を暗い画素に、暗い画素を明るい画素に変換する処理です。"
 
+    #change.html内で変換された画像とメッセージが表示されるようにする
     output_path = os.path.join('static', 'output.png')
     file_exists = os.path.exists(output_path)
-    return render_template('change.html', file_exists=file_exists)
+    return render_template('change.html', file_exists=file_exists, message=conv_message)
     
+
+
 #変換画像ダウンロードようのエンドポイント
 @app.route('/change/dwonload', methods=['POST'])
 def dwonload():
